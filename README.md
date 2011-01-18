f340001bd66faa3303e02dba90a2b5dc396d47ff
awayken <miles@milesrausch.com>
2011-01-18 09:45:03 -0600

    Added demo page, tweaked styling, and passed plugin through JSLint.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 **jQuery Flakes**
 
 A jQuery plugin that will add snowflakes or rainy umbrellas to your website!
+
+Options include:
+
+* type (snow || rain)
+* duration (rate of fall)
+* opacityStart (0 < x < 1)
+* opacityEnd (0 < x < 1)
--- flakes.jquery.js
+++ flakes.jquery.js
@@ -1,25 +1,71 @@
-(function($) {
-	$.fn.flakes=function(o) {
-		o=$.extend({"type":"snow","duration":20,"opacityStart":1,"opacityEnd":0}, o||{});
-		var t="";
-        function createFlake(el,prop) {
-            var sizeclass="",sizecss={"z-index":50};
-            switch(Math.floor(Math.random()*10)) {
-            	case 8: case 9: sizeclass="jqflake_large"; sizecss={"z-index":90}; break;
-            	case 0: case 1: sizeclass="jqflake_small"; sizecss={"z-index":10}; break;
+/*jslint white: true, browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
+/*global $: false, jQuery: false, window: false */
+
+(function ($) {
+	"use strict";
+
+	$.fn.flakes = function (options) {		
+		var o = $.extend({'type': 'snow', 'duration': 20, 'opacityStart': 1, 'opacityEnd': 0}, options || {}), t = '';
+		
+        function createFlake(el, prop) {
+            var sizeclass = '', sizecss = { 'z-index': 50 }, icon = '&#10052;', flake = $('<div />').addClass('flake'), fh = '', fl = '';
+			
+            switch (Math.floor(Math.random() * 10)) {
+			case 8:
+			case 9:
+				sizeclass = 'flake_large';
+				sizecss = { 'z-index': 90 };
+				break;
+			case 0:
+			case 1:
+				sizeclass = 'flake_small';
+				sizecss = { 'z-index': 10 };
+				break;
             }
-	    var icon="&#10052;"
-            if(o.type==="rain"){
-		icon="&#9748;";
-	    }
-            var flake=$("<div>").addClass("jqflake "+sizeclass).css(sizecss).html(icon).appendTo(el).fadeIn("fast"),fh=flake.outerHeight(true),left=Math.floor(Math.random()*(prop.w-flake.width()));
-            flake.css({"top":-1/2*fh+"px","left":left,"opacity":o.opacityStart,"position":"absolute","overflow":"hidden"}).animate({"top":prop.h-fh+"px","opacity":o.opacityEnd}, o.duration*prop.h, "linear", function(){$(this).fadeOut("fast",function(){$(this).remove()});});
-            t = setTimeout(function(){createFlake(el,prop);}, left);
-        };
-		return this.each(function() {
-			var $this=$(this);
-			$this.addClass("jqflakewrap jqflake_"+o.type).css("position","relative");
-			createFlake(this,{"h":$this.height(),"w":$this.width()});
+			
+			if (o.type === 'rain') {
+				icon = '&#9748;';
+			}
+			
+            flake.addClass(sizeclass).css(sizecss).html(icon).appendTo(el).fadeIn("fast");
+
+			fh = flake.outerHeight(true);
+			fl = Math.floor(Math.random() * (prop.w - flake.width()));
+			
+            flake
+				.css({
+					'position': 'absolute',
+					'overflow': 'hidden',
+					'top': (-1 / 2 * fh) + 'px',
+					'left': fl,
+					'opacity': o.opacityStart
+				})
+				.animate({
+					'top': (prop.h - fh) + 'px',
+					'opacity': o.opacityEnd
+					},
+					o.duration * prop.h,
+					'linear',
+					function () {
+						$(this).fadeOut('fast',
+							function () {
+								$(this).remove();
+							}
+						);
+					}
+				);
+			
+            t = setTimeout(function () {
+				createFlake(el, prop);
+			}, fl);
+        }
+		
+		return this.each(function () {
+			var el = $(this);
+			
+			el.addClass('flakes flake_' + o.type).css('position', 'relative');
+			
+			createFlake(this, { 'h': el.height(), 'w': el.width() });
 		});
 	};
-})(jQuery);
+}(jQuery));
